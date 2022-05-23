ce01541f9af477063640412cf84470e1f04bccda
b21eddka <b21eddka@student.his.se>
2022-05-23 19:46:17 +0200

    tillgång till internet, lade till JsonTask, Djur klassen är klar

--- app/src/main/java/com/example/project/Djur.java
+++ app/src/main/java/com/example/project/Djur.java
@@ -1,4 +1,56 @@
 package com.example.project;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Djur {
+
+    private String ID;
+    @SerializedName("Name")
+    private String Namn;
+    private AuxData auxData;
+
+    public class AuxData{
+
+        private String kommentar;
+        private String img;
+        private String skala;
+
+        public AuxData(String kommentar, String img, String skala){
+            this.kommentar = kommentar;
+            this.img = img;
+            this.skala = skala;
+        }
+
+
+        public String getKommentar() {
+            return kommentar;
+        }
+
+        public String getImg() {
+            return img;
+        }
+
+        public String getSkala() {
+            return skala;
+        }
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public String getNamn() {
+        return Namn;
+    }
+
+    public AuxData getAuxData() {
+        return auxData;
+    }
+
+    public Djur(String ID, String Namn, AuxData AuxData){
+        this.ID = ID;
+        this.Namn = Namn;
+        this.auxData = AuxData;
+    }
+
 }
--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,18 +1,34 @@
 package com.example.project;
 
 import android.os.Bundle;
-
+import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+@SuppressWarnings("FieldCanBeLocal")
+public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
+
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=b21eddka";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+
     }
 
+
+    @Override
+    public void onPostExecute(String json) {
+
+    }
 }
+
+
+
